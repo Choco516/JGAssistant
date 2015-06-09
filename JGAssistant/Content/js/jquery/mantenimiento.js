b519e42b2176f5f8170d807e3c55cfa5d3e1981e
Jeankrojas <jcrojasm92@gmail.com>
2015-06-08 19:24:18 -0600

    shiping view

---
+++ JGAssistant/Content/js/jquery/mantenimiento.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    $("#nuevocont").click(function () {
+        $("#divnuevocont").show();
+        $("#divtablacont").hide();
+    });
+
+    $("#guardarcont").click(function () {
+        $("#divtablacont").show();
+        $("#divnuevocont").hide();
+    });
+
+    $("#guardarnav").click(function () {
+        $("#divtablacont").show();
+        $(".blockInput").prop("disabled", true);
+        $('#pais1').prop('disabled', true).trigger("liszt:updated");
+        $("#guardarnav").hide();
+    });
+});
